baceadecd5c85b34f1a5b25c9babb96ced840e2c
Israel Moreno <iml@beonww.com>
2024-11-20 17:03:15 +0100

    ci: Configuración de GitHub Pages
    - Añadido workflow de GitHub Actions para despliegue automático
    - Configurada base URL en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/imorlab-portfolio/',
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
